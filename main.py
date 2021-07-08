97b88599e0561d1b2edefb948cb3b05517b89750
DiegoELT99 <diegoelt99@gmail.com>
2021-07-08 03:26:02 -0500

    Support for messages of len 158

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import keys
 import encrypt
-import codecs
+import base64
 
 testfile = "test.pem"
 keys.create_private_key_file(testfile)
 
-message = "Test Message 1"
-message2 = "Test Message 2"
+message = "Test Message"
 
 private_key = keys.read_private_key(testfile)
 public_key = keys.get_public_from_private(private_key)
 
-encrypted = encrypt.encrypt_with_key(public_key, message)
-encrypted2 = encrypt.encrypt_with_key(public_key, message2)
+encrypted = encrypt.encrypt_with_key(public_key, message) # Currently only working until 158 characters per message
 
-print(len(encrypted))
-print(len(encrypted2))
+encoded = base64.b85encode(encrypted)
+test = base64.b85decode(encoded)
 
-hex1 = encrypted.hex()
+#owo = base64.b85encode(encrypted)
+#print(g(str(hex1)))
 
-print(encrypt.decrypt_with_key(private_key, encrypted))
+print(encrypt.decrypt_with_key(private_key, test))
