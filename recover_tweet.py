e69223f5b93f04dfece6ed0aa7c40ae8e8d47f9c
L4mb0 <juan.navarro@utec.edu.pe>
2021-08-05 18:13:15 -0500

    flask done

--- recover_tweet.py
+++ recover_tweet.py
@@ -6,49 +6,70 @@ from twython import Twython
 import base64
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
+from flask import Flask, request, render_template
 
-testfile = "test.pem"
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def recover_tweets():
+    testfile = "test.pem"
 
 
-private_key = keys.read_private_key(testfile)
+    private_key = keys.read_private_key(testfile)
 
-with open("public_key.pem", "rb") as key_file:
-    public_key = serialization.load_pem_public_key(
+    with open("public_key.pem", "rb") as key_file:
+        public_key = serialization.load_pem_public_key(
             key_file.read(),
             backend=default_backend()
             )
 
-load_dotenv()
+    load_dotenv()
 
-APP_KEY = os.getenv('APP_KEY')
-APP_SECRET = os.getenv('APP_SECRET')
-OAUTH_TOKEN = os.getenv('OAUTH_TOKEN')
-OAUTH_TOKEN_SECRET = os.getenv('OAUTH_TOKEN_SECRET')
+    APP_KEY = os.getenv('APP_KEY')
+    APP_SECRET = os.getenv('APP_SECRET')
+    OAUTH_TOKEN = os.getenv('OAUTH_TOKEN')
+    OAUTH_TOKEN_SECRET = os.getenv('OAUTH_TOKEN_SECRET')
 
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-print('Keys recovered succesfully')
+    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    print('Keys recovered succesfully')
 
-##########################
+    ##########################
 
-timeline = twitter.get_user_timeline(screen_name='fakest_ever',
-        count=1, tweet_mode='extended')
+    timeline = twitter.get_user_timeline(screen_name='fakest_ever',
+        count=5, tweet_mode='extended')
 
-print('Timeline recovered')
-
-ourtweet = timeline[0]['full_text']
-#decoded = bytearray(ourtweet)
-
-ourtweet = ourtweet.replace('&gt;','>')
-ourtweet = ourtweet.replace('&lt;','<')
-ourtweet = ourtweet.replace('amp;','')
-
-ourtweet = ourtweet.replace('á','@')
-ourtweet = ourtweet.replace('é','#')
-ourtweet = ourtweet.replace('ó','.')
+    print('Timeline recovered successfully')
 
 
-print('Lastest tweet recovered')
+    enc = []
+    for i in range(5):
+        ourtweet = timeline[i]['full_text']
 
-decoded = base64.b85decode(ourtweet)
-print("decrypted tweet: ", encrypt.decrypt_with_key(private_key, decoded))
+        ourtweet = ourtweet.replace('&gt;','>')
+        ourtweet = ourtweet.replace('&lt;','<')
+        ourtweet = ourtweet.replace('amp;','')
 
+        ourtweet = ourtweet.replace('á','@')
+        ourtweet = ourtweet.replace('é','#')
+        ourtweet = ourtweet.replace('ó','.')
+        enc.append(ourtweet)
+
+
+    print('Lastest 5 tweets recovered')
+
+
+    dec = []
+    for i in range(5):
+        decoded = base64.b85decode(enc[i])
+        decoded = encrypt.decrypt_with_key(private_key, decoded)
+        dec.append(decoded)
+    
+
+    all_tweets = (zip(enc,dec))
+
+    print(all_tweets)
+
+    return render_template('recover_tweets.html', data=all_tweets)
+
+
+app.run(host='localhost', port='5000')
--- send_tweet.py
+++ send_tweet.py
@@ -6,46 +6,59 @@ from twython import Twython
 import base64
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
+from flask import Flask, request, render_template
 
-testfile = "test.pem"
+app = Flask(__name__)
 
-message = input()
+@app.route('/')
+def box():
+    return render_template('send_tweet.html')
 
+@app.route('/', methods=['POST', 'GET'])
+def send_tweet():
+    if request.method == 'POST':
+        print('Starting process...')
 
-private_key = keys.read_private_key(testfile)
+        testfile = "test.pem"
 
-with open("public_key.pem", "rb") as key_file:
-    public_key = serialization.load_pem_public_key(
-            key_file.read(),
-            backend=default_backend()
+        message = request.form['tweet']
+
+        private_key = keys.read_private_key(testfile)
+
+        with open("public_key.pem", "rb") as key_file:
+            public_key = serialization.load_pem_public_key(
+                key_file.read(),
+                backend=default_backend()
             )
 
-encrypted = encrypt.encrypt_with_key(public_key, message) # Currently only working until 158 characters per message
+        encrypted = encrypt.encrypt_with_key(public_key, message) # Currently only working until 158 characters per message
 
-print("Messaged cyphered succesfully")
+        print("Messaged cyphered succesfully")
 
 #########################
 
-load_dotenv()
+        load_dotenv()
 
-APP_KEY = os.getenv('APP_KEY')
-APP_SECRET = os.getenv('APP_SECRET')
-OAUTH_TOKEN = os.getenv('OAUTH_TOKEN')
-OAUTH_TOKEN_SECRET = os.getenv('OAUTH_TOKEN_SECRET')
+        APP_KEY = os.getenv('APP_KEY')
+        APP_SECRET = os.getenv('APP_SECRET')
+        OAUTH_TOKEN = os.getenv('OAUTH_TOKEN')
+        OAUTH_TOKEN_SECRET = os.getenv('OAUTH_TOKEN_SECRET')
 
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-print('Keys recovered succesfully')
+        twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+        print('Keys recovered succesfully')
 
-encoded = base64.b85encode(encrypted)
+        encoded = base64.b85encode(encrypted)
 
 ##########################
 
-repl = str(encoded).replace('@','á')
-repl = repl.replace('#','é')
-repl = repl.replace('.','ó')
-repl = repl[2:-1]
+        repl = str(encoded).replace('@','á')
+        repl = repl.replace('#','é')
+        repl = repl.replace('.','ó')
+        repl = repl[2:-1]
 
-twitter.update_status(status=repl)
-print("Tweet posted")
+        twitter.update_status(status=repl)
+        print("Tweet posted")
+    
+    return render_template('send_tweet.html', data=repl)
 
-
+app.run(host='localhost', port=5000)
